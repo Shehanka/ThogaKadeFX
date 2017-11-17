d8251ed8ca57def7f3596d030b3de3b7e7c0e8ed
Shehanka <hcsperera@outlook.com>
2017-11-17 14:43:00 +0530

    updated packages and utils

---
+++ ThogaKadeCommon/src/com/chamodshehanka/thogaKadeCommon/observer/ItemObserver.java
@@ -0,0 +1,11 @@
+package com.chamodshehanka.thogaKadeCommon.observer;
+
+import java.rmi.Remote;
+
+/**
+ * @author chamodshehanka on 11/17/2017
+ * @project ThogaKadeFX
+ **/
+public interface ItemObserver extends Remote{
+
+}
